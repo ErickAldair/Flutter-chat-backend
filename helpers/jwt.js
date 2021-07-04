9cbabb1dd9c93ea510e60aa2922f8235ce5f22f4
ErickAldair <aldair-gym97@gmail.com>
2021-07-04 09:13:53 -0500

    fin seción 8

---
+++ helpers/jwt.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = (uid) => {
+
+   return new Promise((resolve, reject)=>{
+
+    const payload = { uid }
+
+    jwt.sign(payload, process.env.JWT_KEY, {
+        expiresIn: '24h'
+    }, (err, token) => {
+        
+     if(err){
+ 
+         //no se pudo crear el token
+         reject('no se puedo generar el JWT');
+ 
+     }else{
+         //token
+         resolve(token);
+     }
+ 
+    })
+   });
+
+
+}
+
+
+module.exports = {
+    generarJWT
+}
